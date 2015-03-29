048e3beac1dc96de3028a7fab90ddebb8c46021b
paqure <dev@paqure.com>
2015-03-28 19:18:46 -0600

    Create abstract Object class

---
+++ abs.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * PaQuRe ABSTRACT CLASSES
+ * @package   paqure
+ * @version   0.0.2
+ * @author    Roderic Linguri
+ * @copyright Copyright (c) 2015, Digices
+ * @license   http://opensource.org/licenses/gpl-license.php GNU Public License
+ */
+
+namespace paqure;
+
+/**
+ * Object
+ */
+abstract class Obj
+{
+
+    // global properties and methods
+
+}
+
+
